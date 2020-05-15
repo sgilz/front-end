ca652fd582253d3ea0da8240fc687f87ead1b38f
Santiago Gil <sgilz@eafit.edu.co>
2020-05-15 08:42:24 -0500

    agregando campos para conexión con bd

--- measure/views.py
+++ measure/views.py
@@ -7,10 +7,13 @@ def measure(request):
     # Verifica si hay un parámetro value en la petición GET
     if 'value' in request.GET:
         value = request.GET['value']
+        lat = request.GET['latitude']
+        length = request.GET['length']
+        ground  = request.GET['grounds']
         # Verifica si el value no esta vacio
-        if value:
+        if value and lat and length and ground:
             # Crea el json para realizar la petición POST al Web Service
-            args = {'type': 'ph', 'value': value}
+            args = {'type': 'ph', 'value': value, 'latitude': lat, 'length' : length, 'ground': ground}
             response = requests.post('http://127.0.0.1:8000/hygrometer/', args)
             # Convierte la respuesta en JSON
             measure_json = response.json()
