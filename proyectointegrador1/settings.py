4a12f82a987d0027fa63c382033c9f7321efebd5
Santiago Gil Zapata <gzsantiago22@outlook.com>
2020-05-27 14:26:14 -0500

    modificando archivo de configuración

--- proyectointegrador1/settings.py
+++ proyectointegrador1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e6(j^pv_^49-#=17*2-qw!-6h&%n9bwv@v5y_*u3j=_b0o9m+&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
