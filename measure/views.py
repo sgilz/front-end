a267f1baf13b030bf35e910a561de2205b989f17
Santiago Gil Zapata <gzsantiago22@outlook.com>
2020-05-27 14:14:31 -0500

    Conectando frontend a backend remoto

--- measure/views.py
+++ measure/views.py
@@ -14,12 +14,12 @@ def measure(request):
         if value and lat and length and ground:
             # Crea el json para realizar la petición POST al Web Service
             args = {'type': 'ph', 'value': value, 'latitude': lat, 'length' : length, 'ground': ground}
-            response = requests.post('http://127.0.0.1:8000/hygrometer/', args)
+            response = requests.post("http://pi1-eafit-sgilz.azurewebsites.net/hygrometer/", args)
             # Convierte la respuesta en JSON
             measure_json = response.json()
 
     # Realiza una petición GET al Web Services
-    response = requests.get('http://127.0.0.1:8000/hygrometer/')
+    response = requests.get("http://pi1-eafit-sgilz.azurewebsites.net/hygrometer/")
     # Convierte la respuesta en JSON
     measures = response.json()
     # Rederiza la respuesta en el template measure
